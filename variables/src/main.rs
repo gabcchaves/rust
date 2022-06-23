702d54eb82c2514583c65be9b99ba127c6ae210d
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-23 12:17:25 -0300

    Shadowing.

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,11 +1,6 @@
 fn main() {
-    let x = 5;
-    let x = x + 1;
+    let spaces = "    ";
+    let spaces = spaces.len();
 
-    {
-        let x = x * 2;
-        println!("The value of x in the inner scope is: {}", x);
-    }
-
-    println!("The value of x is: {}", x);
+    println!("{}", spaces);
 }

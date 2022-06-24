abf4eef4226c338fc4120098e2c89b7561f787f1
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-06-23 21:53:49 -0300

    Integer data type.

---
+++ data_types/src/main.rs
@@ -0,0 +1,39 @@
+fn main() {
+    /* Signed integers */
+    // 8-bit signed integer
+    let _x: i8 = 10;
+
+    // 16-bit signed integer
+    let _x: i16 = 10;
+
+    // 32-bit signed integer
+    let _x: i32 = 10;
+
+    // 64-bit signed integer
+    let _x: i64 = 10;
+
+    // 128-bit signed integer;
+    let _x: i128 = 10;
+
+    // architecture's default size
+    let _x: isize = 10;
+    
+    /* Unsigned integers */
+    // 8-bit unsigned integer
+    let _y: u8 = 10;
+
+    // 16-bit unsigned integer
+    let _y: u16 = 10;
+
+    // 32-bit unsigned integer
+    let _y: u32 = 10;
+
+    // 64-bit unsigned integer
+    let _y: u64 = 10;
+
+    // 128-bit unsigned integer
+    let _y: u128 = 10;
+
+    // architecture's default size
+    let _y: usize = 10;
+}

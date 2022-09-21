adccf311066502ee6ab0d556fc214afb96000aea
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-21 09:32:06 -0300

    Quadratic equation calculator.

--- stdio/src/bin/quadratic_equation.rs
+++ stdio/src/bin/quadratic_equation.rs
@@ -2,35 +2,43 @@ use std::io;
 use std::str::FromStr;
 
 fn main() {
-    let scn = io::stdin();
-    let (mut a, mut b, mut c) = (
-        String::new(),
-        String::new(),
-        String::new()
-    );
+    println!("Enter the value of a: ");
+    let a: f64 = {
+        let mut input = read_float();
+        while input == 0.0 {
+            println!("Enter a non-zero value for a: ");
+            input = read_float();
+        }
+        input
+    };
 
-    loop {
-        match scn.read_line(&mut a) {
-            Ok(..) => {}
-            Err(error) => {
-                println!("Error: {error}");
-            }
-        }
-        if f64::from_str(&a).is_ok() {
-            break;
-        }
-    }
+    println!("Enter the value of b: ");
+    let b: f64 = read_float();
 
-    match scn.read_line(&mut b) {
-        Ok(..) => {}
-        Err(error) => {
-            println!("Error: {error}");
-        }
+    println!("Enter the value of c: ");
+    let c: f64 = read_float();
+
+    let d: f64 = b.powi(2) - 4.0 * a * c;
+    let x1: f64 = ( -b + d.sqrt() ) / ( 2.0 * a );
+    let x2: f64 = ( -b - d.sqrt() ) / ( 2.0 * a );
+
+    println!("Results:");
+    if x1 == f64::NAN {
+        println!("X1: Non real");
+    } else {
+        println!("{}", x1);
     }
-    match scn.read_line(&mut c) {
-        Ok(..) => {}
-        Err(error) => {
-            println!("Error: {error}");
-        }
+    if x2 == f64::NAN {
+        println!("X2: Non real");
+    } else {
+        println!("{}", x2);
     }
+}
+
+fn read_float() -> f64 {
+    let mut str_input = String::new();
+    let mut num: f64;
+    io::stdin().read_line(&mut str_input);
+    num = f64::from_str(&str_input.trim()).unwrap();
+    num
 }

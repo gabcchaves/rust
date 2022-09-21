cd759886d79e4dd9846a321ce803b24565401244
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-09-21 16:28:02 -0300

    Strings 3.

--- rustlings/exercises/strings/strings3.rs
+++ rustlings/exercises/strings/strings3.rs
@@ -1,21 +1,19 @@
 // strings3.rs
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
 
 fn trim_me(input: &str) -> String {
-    // TODO: Remove whitespace from the end of a string!
-    ???
+    String::from(input.trim())
 }
 
 fn compose_me(input: &str) -> String {
-    // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut str_input = String::from(input);
+    str_input.push_str(" world!");
+    str_input
 }
 
 fn replace_me(input: &str) -> String {
-    // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    String::from(input.replace("cars", "balloons"))
 }
 
 #[cfg(test)]

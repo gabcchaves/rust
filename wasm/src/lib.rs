82abfff00bb0740e550bf9cfe65b1bdd757eab0c
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-25 10:58:06 -0300

    Log.

--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -1,14 +1,7 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use wasm_bindgen::prelude::*;
+use web_sys::prelude::*;
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+#[wasm_bindgen]
+pub fn log(s: &str) {
+    console_log(&format!("{}", s));
 }

279d817cfdc6b2e36fe33c6f2047556bda2f74bc
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-10-25 10:49:44 -0300

    Set up Cargo.

---
+++ wasm/target/debug/deps/unicode_ident-cfa2456f7f726e1a.d
@@ -0,0 +1,8 @@
+/home/gabriel/Dev/remote/rust/wasm/target/debug/deps/unicode_ident-cfa2456f7f726e1a.rmeta: /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/lib.rs /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/tables.rs
+
+/home/gabriel/Dev/remote/rust/wasm/target/debug/deps/libunicode_ident-cfa2456f7f726e1a.rlib: /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/lib.rs /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/tables.rs
+
+/home/gabriel/Dev/remote/rust/wasm/target/debug/deps/unicode_ident-cfa2456f7f726e1a.d: /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/lib.rs /home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/tables.rs
+
+/home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/lib.rs:
+/home/gabriel/.cargo/registry/src/github.com-1ecc6299db9ec823/unicode-ident-1.0.5/src/tables.rs:

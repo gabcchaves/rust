caed3b0ad81c0e9dafeb23f5a093f2afef471ed4
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-02 13:10:56 -0300

    Component with properties.

--- web/components/src/main.rs
+++ web/components/src/main.rs
@@ -1,7 +1,16 @@
 use yew::prelude::*;
 
+/* Properties */
+#[derive(PartialEq, Properties)]
+struct Props {
+    id: String,
+    children: Children,
+}
+
+/* Components */
 struct Container;
 struct ComponentNoProps;
+struct ComponentWithProps;
 
 impl Component for Container {
     type Message = ();
@@ -15,6 +24,10 @@ impl Component for Container {
         html! {
             <>
                 <ComponentNoProps />
+                <br/><br/>
+                <ComponentWithProps id="1">
+                    <p>{"This component has two properties."}</p>
+                </ComponentWithProps>
             </>
         }
     }
@@ -31,6 +44,24 @@ impl Component for ComponentNoProps {
     fn view(&self, _ctx: &Context<Self>) -> Html {
         html! {
             {"This component has no properties."}
+        }
+    }
+}
+
+impl Component for ComponentWithProps {
+    type Message = ();
+    type Properties = Props;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <div id={ctx.props().id.clone()}>
+                <strong>{format!("Component - {}", ctx.props().id.clone())}</strong>
+                { ctx.props().children.clone() }
+            </div>
         }
     }
 }

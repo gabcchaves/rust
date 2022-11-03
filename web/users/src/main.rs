bf44d692644aba94cd07a4a4517ee818cd11285e
Gabriel Costa Chaves <gabcchaves@zohomail.com>
2022-11-03 11:40:27 -0300

    Section toggling page, without reloading.

--- web/users/src/main.rs
+++ web/users/src/main.rs
@@ -1,38 +1,58 @@
 use yew::prelude::*;
 
-enum Msg {
+enum MsgSection {
+    Main,
     Login,
     Register,
 }
-
 
 struct App {
     section: u8,
 }
 
 struct Header;
+struct Main;
+struct Login;
+struct Register;
+
 #[derive(PartialEq, Properties)]
 struct HeaderProps {
     children: Children,
 }
+#[derive(PartialEq, Properties)]
+struct MainProps {
+    children: Children,
+}
+#[derive(PartialEq, Properties)]
+struct LoginProps {
+    children: Children,
+}
+#[derive(PartialEq, Properties)]
+struct RegisterProps {
+    children: Children,
+}
 
 impl Component for App {
-    type Message = Msg;
+    type Message = MsgSection;
     type Properties = ();
-
+    
     fn create(_ctx: &Context<Self>) -> Self {
         Self {
-            section: 0
+            section: 0,
         }
     }
 
     fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
         match msg {
-            Msg::Login => {
+            MsgSection::Main => {
+                self.section = 0;
+                true
+            },
+            MsgSection::Login => {
                 self.section = 1;
                 true
             },
-            Msg::Register => {
+            MsgSection::Register => {
                 self.section = 2;
                 true
             }
@@ -44,13 +64,38 @@ impl Component for App {
         html! {
             <>
                 <Header>
-                    <button onclick={link.callback(|_| Msg::Login)}>{"Login"}</button>
-                    <button onclick={link.callback(|_| Msg::Register)}>{"Register"}</button>
+                    <div class="login-area">
+                        <h1 onclick={link.callback(|_| MsgSection::Main)}>{"Compraqui"}</h1>
+                    </div>
+                    <div class="log-options">
+                        <button onclick={link.callback(|_| MsgSection::Login)}>{"Login"}</button>
+                        <button onclick={link.callback(|_| MsgSection::Register)}>{"Register"}</button>
+                    </div>
                 </Header>
-                if self.section == 1 {
-                    <h1>{"Tela de LogIn"}</h1>
+                if self.section == 0 {
+                    <Main>
+                        <h2>{"Página principal"}</h2>
+                    </Main>
+                } else if self.section == 1 {
+                    <Login>
+                        <h2>{"Página de login"}</h2>
+                        <form method="POST">
+                            <input type="email" placeholder="E-mail"/>
+                            <input type="password" placeholder="Senha"/>
+                            <input type="submit" value="Entrar"/>
+                        </form>
+                    </Login>
                 } else if self.section == 2 {
-                    <h1>{"Tela de Cadastro"}</h1>
+                    <Register>
+                        <h2>{"Página de cadastro"}</h2>
+                        <form method="POST">
+                            <input type="text" placeholder="Nome completo"/>
+                            <input type="email" placeholder="Endereço de e-mail"/>
+                            <input type="password" placeholder="Senha"/>
+                            <input type="password" placeholder="Confirmar senha"/>
+                            <input type="submit" value="Cadastrar"/>
+                        </form>
+                    </Register>
                 }
             </>
         }
@@ -68,8 +113,61 @@ impl Component for Header {
     fn view(&self, ctx: &Context<Self>) -> Html {
         html! {
             <header>
-                { ctx.props().children.clone() }
+                <>
+                    { ctx.props().children.clone() }
+                </>
             </header>
+        }
+    }
+}
+
+impl Component for Main {
+    type Message = ();
+    type Properties = MainProps;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                { ctx.props().children.clone() }
+            </>
+        }
+    }
+}
+
+impl Component for Login {
+    type Message = ();
+    type Properties = LoginProps;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                { ctx.props().children.clone() }
+            </>
+        }
+    }
+}
+
+impl Component for Register {
+    type Message = ();
+    type Properties = RegisterProps;
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        html! {
+            <>
+                { ctx.props().children.clone() }
+            </>
         }
     }
 }

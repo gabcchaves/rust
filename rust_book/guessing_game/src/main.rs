2b39c78703cffb588586b9ebb4d4df5314238dc5
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-15 18:33:37 -0300

    Number comparison; variable shadowing.

--- rust_book/guessing_game/src/main.rs
+++ rust_book/guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess the number!");
@@ -14,8 +15,16 @@ fn main() {
         .read_line(&mut guess)  // Method called on the returned handle.
         .expect("Failed to read line.");
     // The &mut indicates that the reference is mutable.
+    
+    let guess: u32 = guess.trim().parse().expect("Enter a number!");
 
     println!("You guessed: {guess}.");
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 
     println!("The secret number is {secret_number}.");
 }

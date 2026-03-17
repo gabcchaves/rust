a727010d34fe1fc3396858e0f4cc7f37c21516cd
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-17 19:56:00 -0300

    Does Rust's compiler allow this operation with two different types of variables?

--- rust_book/variables/src/main.rs
+++ rust_book/variables/src/main.rs
@@ -4,7 +4,13 @@ fn main() {
     //println!("Value of x is {x}");
     //// It throws an error, indicating that u8 variables shall be in the range of 0..=255.
 
-    // Which data type would Rust compiler infer?
-    let x = 3.1415;
-    println!("Compiler infered that x is of type {}.", std::any::type_name_of_val(&x));
+    //// Which data type would Rust compiler infer?
+    //let x = 3.1415;
+    //println!("Compiler infered that x is of type {}.", std::any::type_name_of_val(&x));
+
+    // Now, let's try operating with variables of different types.
+    let x: u8 = 10;
+    let w: f32 = 2.5;
+    let r = x + w;
+    println!("{r}");
 }

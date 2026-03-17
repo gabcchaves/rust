cd28ef79382b055d73d4217676ef038f871019b2
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-17 19:50:43 -0300

    Figured out which floating point type Rust's compiler infers from an initialized unannotated float variable.

--- rust_book/variables/src/main.rs
+++ rust_book/variables/src/main.rs
@@ -1,6 +1,10 @@
 fn main() {
-    // Testing interger overflow.
-    let x: u8 = 256;
-    println!("Value of x is {x}");
-    // It throws an error, indicating that u8 variables shall be in the range of 0..=255.
+    //// Testing interger overflow.
+    //let x: u8 = 256;
+    //println!("Value of x is {x}");
+    //// It throws an error, indicating that u8 variables shall be in the range of 0..=255.
+
+    // Which data type would Rust compiler infer?
+    let x = 3.1415;
+    println!("Compiler infered that x is of type {}.", std::any::type_name_of_val(&x));
 }

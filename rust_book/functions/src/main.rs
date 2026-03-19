ca37da85191db96af50667aed98ac248408f718a
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-19 09:29:15 -0300

    2. Parâmetros e tipagem.

--- rust_book/functions/src/main.rs
+++ rust_book/functions/src/main.rs
@@ -1,8 +1,13 @@
 /* 1. Sintaxe e declaração de funções em Rust. */
-fn saudar() {
-    println!("Olá.");
+//fn saudar() {
+//    println!("Olá.");
+//}
+
+/* 2. Parâmetros e tipagem. */
+fn exibir_idade(idade: u32) {
+    println!("A idade informada é: {} anos.", idade);
 }
 
 fn main() {
-    saudar();
+    exibir_idade(24);
 }

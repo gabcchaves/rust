3dfcae187bdcf757e6f13c02a1e6ec28ee4b50f2
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-19 09:38:06 -0300

    4. Instrução vs expressão.

--- rust_book/functions/src/main.rs
+++ rust_book/functions/src/main.rs
@@ -9,10 +9,17 @@
 //}
 
 /* 3. Múltiplos parâmetros. */
-fn imprimir_medidas(valor: f64, unidade: char) {
-    println!("A medida é {}{}.", valor, unidade);
-}
+//fn imprimir_medidas(valor: f64, unidade: char) {
+//    println!("A medida é {}{}.", valor, unidade);
+//}
+
+/* 4. Instrução vs expressão. */
+/* Qual é a diferença fundamental entre uma instrução e uma expressão? */
+// A diferença é que uma instrução não retorna valor algum, enquanto que uma expressão sempre
+// retorna algum valor.
+/* Por que não se pode atribuir uma intrução a outra instrução? [e.g.: let x = (let y = 10)]; */
+// Justamente pelo fato de que uma instrução não retorna um valor. Como não retorna valor, não se
+// pode atribuí-la uma outra instrução ou até mesmo a uma expressão.
 
 fn main() {
-    imprimir_medidas(5.0, 'm');
 }

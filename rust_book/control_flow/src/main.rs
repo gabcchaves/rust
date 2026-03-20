abdfb904cfc1578a6bf43fb10799d4ad11d6fcca
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-20 10:14:49 -0300

    2. Múltiplas condições.

--- rust_book/control_flow/src/main.rs
+++ rust_book/control_flow/src/main.rs
@@ -1,9 +1,27 @@
-fn main() {
-    let numero = 12;
+/* 1. Condicional básica. */
+//fn test() {
+//    let numero = 12;
+//
+//    if numero > 10 {
+//        println!("O número é maior que 10.");
+//    } else {
+//        println!("O número é menor que 10.");
+//    }
+//}
 
-    if numero > 10 {
-        println!("O número é maior que 10.");
+/* 2. Múltiplas condições. */
+fn test(nota: u8) -> char {
+    if nota >= 90 {
+        'A'
+    } else if nota >= 80 {
+        'B'
+    } else if nota >= 70 {
+        'C'
     } else {
-        println!("O número é menor que 10.");
+        'D'
     }
+}
+
+fn main() {
+    println!("{}", test(100));
 }

beb2905ee87b9631a6fda1c5f2727e66a03ed1c2
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-20 10:35:29 -0300

    3. if em uma instrução let.

--- rust_book/control_flow/src/main.rs
+++ rust_book/control_flow/src/main.rs
@@ -10,18 +10,32 @@
 //}
 
 /* 2. Múltiplas condições. */
-fn test(nota: u8) -> char {
-    if nota >= 90 {
-        'A'
-    } else if nota >= 80 {
-        'B'
-    } else if nota >= 70 {
-        'C'
+//fn test(nota: u8) -> char {
+//    if nota >= 90 {
+//        'A'
+//    } else if nota >= 80 {
+//        'B'
+//    } else if nota >= 70 {
+//        'C'
+//    } else {
+//        'D'
+//    }
+//}
+
+/* 3. if em uma intrução let */
+// Considerando que if é uma expressão, então if retorna um valor e, portanto, pode ser usado em
+// uma instrução let.
+fn test() -> String {
+    let condicao: bool = true;
+    let resultado = if condicao {
+        String::from("Sim")
     } else {
-        'D'
-    }
+        String::from("Não")
+    };
+
+    resultado
 }
 
 fn main() {
-    println!("{}", test(100));
+    println!("{}", test());
 }

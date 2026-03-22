1eef717db4872a12b9aebef6c07c96d3474d36e2
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-22 15:44:58 -0300

    Removed unnecessary compilation results from remote repository.

--- rust_book/control_flow/target/debug/control_flow.d
+++
@@ -1,1 +0,0 @@
-/home/gabrielchaves/Projetos/remote/rust/rust_book/control_flow/target/debug/control_flow: /home/gabrielchaves/Projetos/remote/rust/rust_book/control_flow/src/main.rs
--- rust_book/control_flow/target/debug/deps/control_flow-d27009c54f591e74.d
+++
@@ -1,5 +0,0 @@
-/home/gabrielchaves/Projetos/remote/rust/rust_book/control_flow/target/debug/deps/control_flow-d27009c54f591e74.d: src/main.rs
-
-/home/gabrielchaves/Projetos/remote/rust/rust_book/control_flow/target/debug/deps/control_flow-d27009c54f591e74: src/main.rs
-
-src/main.rs:

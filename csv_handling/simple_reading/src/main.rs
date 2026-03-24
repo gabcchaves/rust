e5a84316ed9da2ee1386ba0e400d0782eb96b1c9
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-24 20:47:22 -0300

    Leitura de uma linha apenas.

--- csv_handling/simple_reading/src/main.rs
+++ csv_handling/simple_reading/src/main.rs
@@ -1,6 +1,7 @@
 use std::error::Error;
 use csv::Reader;
 use csv::ReaderBuilder;
+use csv::StringRecord;
 /* 2. Leitura básica. */
 // As estruturas principais do crate CSV são o Reader e Writer.
 // Função para abrir arquivo e imprimir conteúdo na saída padrão.
@@ -41,6 +42,7 @@ fn read_semicolon(path: String) -> Result<(), Box<dyn Error>> {
 }
 
 
+#[allow(dead_code)]
 #[allow(unused_variables)]
 fn read_ignore_headers(path: String) -> Result<(), Box<dyn Error>> {
     // Ignorar cabeçalhos na leitura.
@@ -57,8 +59,37 @@ fn read_ignore_headers(path: String) -> Result<(), Box<dyn Error>> {
 }
 
 
+// Ler uma única linha.
+fn read_line(path: String) -> Result<(), Box<dyn Error>> {
+    let mut rdr = Reader::from_path(path)?;
+    
+    let mut record = StringRecord::new();
+
+    //// read_record retorna valor booleano.
+    //if rdr.read_record(&mut record)? {
+    //    println!("{:?}", record);
+    //    Ok(())
+    //} else {
+    //    Err((error))
+    //}
+    
+    // ERRADO:
+    //match rdr.read_record(&mut record) {
+    //    Ok(record) => {
+    //        println!("{:?}", record);
+    //    },
+    //    Err(error) => return Err(error),
+    //}
+
+    rdr.read_record(&mut record)?;
+    println!("{:?}", record);
+
+    Ok(())
+}
+
+
 fn main() {
-    let result = read_ignore_headers(String::from("foo.csv"));
+    let result = read_line(String::from("foo.csv"));
 
     // Tratamento de erro.
     match result {

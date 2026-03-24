b2ae678d94e6f07c810eea1f04aa92e47e2760f5
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-24 19:56:15 -0300

    Abertura do arquivo; arquivo inexistente.

--- csv_handling/simple_reading/src/main.rs
+++ csv_handling/simple_reading/src/main.rs
@@ -1,3 +1,28 @@
+use std::error::Error;
+use csv::Reader;
+/* 2. Leitura básica. */
+// As estruturas principais do crate CSV são o Reader e Writer.
+// Função para abrir arquivo e imprimir conteúdo na saída padrão.
+//
+// As implementações principais da estrutura Reader são:
+// - from_path
+// - from_reader
+
+#[allow(unused_variables)]
+fn open_file(path: String) -> Result<(), Box<dyn Error>> {
+    let rdr = Reader::from_path(path)?;
+    Ok(())
+}
+
+
 fn main() {
-    println!("Hello, world!");
+    let result = open_file(String::from("foo.csv"));
+    match result {
+        Ok(_file) => {
+            println!("OK.");
+        },
+        Err(error) => {
+            println!("Error: {}", error);
+        },
+    };
 }

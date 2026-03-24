ed0cdcbc045c1bc2e71a399e8fc4828b7f83a9ff
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-24 20:26:30 -0300

    4. Delimitador customizado.

--- csv_handling/simple_reading/src/main.rs
+++ csv_handling/simple_reading/src/main.rs
@@ -1,5 +1,6 @@
 use std::error::Error;
 use csv::Reader;
+use csv::ReaderBuilder;
 /* 2. Leitura básica. */
 // As estruturas principais do crate CSV são o Reader e Writer.
 // Função para abrir arquivo e imprimir conteúdo na saída padrão.
@@ -8,15 +9,29 @@ use csv::Reader;
 // - from_path
 // - from_reader
 
+#[allow(dead_code)]
 #[allow(unused_variables)]
 fn open_file(path: String) -> Result<(), Box<dyn Error>> {
-    let mut rdr = Reader::from_path(path)
-        .expect("Erro ao ler");
+    let mut rdr = Reader::from_path(path)?;
     
     // método records retorna iterador com os registros do arquivo.
     for result in rdr.records() {
-        let record = result
-            .expect("Erro ao ler");
+        let record = result?;
+        println!("{:?}", record);
+    }
+
+    Ok(())
+}
+
+#[allow(unused_variables)]
+fn read_semicolon(path: String) -> Result<(), Box<dyn Error>> {
+    // Leitura de arquivo CSV cujo separador é ';'.
+    let mut rdr = ReaderBuilder::new()
+        .delimiter(b';')
+        .from_path(path)?;
+
+    for result in rdr.records() {
+        let record = result?;
         println!("{:?}", record);
     }
 
@@ -25,16 +40,15 @@ fn open_file(path: String) -> Result<(), Box<dyn Error>> {
 
 
 fn main() {
-    let result = open_file(String::from("foo.csv"))
-        .expect("Erro ao ler");
+    let result = read_semicolon(String::from("foo2.csv"));
 
-    //// Tratamento de erro.
-    //match result {
-    //    Ok(_file) => {
-    //        println!("OK.");
-    //    },
-    //    Err(error) => {
-    //        println!("Error: {}", error);
-    //    },
-    //};
+    // Tratamento de erro.
+    match result {
+        Ok(_file) => {
+            println!("OK.");
+        },
+        Err(error) => {
+            println!("Error: {}", error);
+        },
+    };
 }

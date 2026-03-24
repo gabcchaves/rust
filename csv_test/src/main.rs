20bb2a30f195e5515de829b859e2d7b3f77fe235
Gabriel Costa Chaves <gccgabr@pm.me>
2026-03-24 17:14:34 -0300

    Creating csv file.

--- csv_test/src/main.rs
+++ csv_test/src/main.rs
@@ -1,13 +1,34 @@
 #![allow(unused)]
 use csv::Reader;
+use csv::Writer;
 use std::error::Error;
 
+
+/* Structs used */
+// Reader //
+// - from_path()
+//
+// Writer //
+// - from_path()
+
 fn open_csv() {
-    let rdr = Reader::from_path("foo.csv");
-    let _rdr_result = match rdr {
-        Ok(file) => file,
-        Err(error) => panic!("Arquivo inexistente:\n\t{}", error),
+//    let rdr = Reader::from_path("foo.csv");
+    let mut wtr = Writer::from_path("foo.csv");
+    match wtr {
+        Ok(file) => {
+            println!("Arquivo criado.");
+            file
+        },
+        Err(error) => todo!(),
     };
+    //let _rdr_result = match rdr {
+    //    Ok(file) => file,
+    //    Err(error) => {
+    //        //wtr.write_record(&["a", "b", "c"]);  
+    //        //wtr.write_record(&["x", "y", "z"]);
+    //        //wtr.flush();
+    //    },
+    //};
 }
 
 fn main() {
